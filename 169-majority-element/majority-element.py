f01643f83bf8658b2b3951f11e7f6eae113857c5
Manikantanallamati <140334586+Manikantanallamati@users.noreply.github.com>
2024-01-07 15:50:03 +0530

    Time: 148 ms (61.75%) | Memory: 18.9 MB (10.81%) - LeetSync

---
+++ 169-majority-element/majority-element.py
@@ -0,0 +1,16 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        a = list(set(nums))
+        b = []
+        c = []
+        for i in range(len(a)):
+            if nums.count(a[i])>=(nums.count(a[i])//2):
+                b.append(a[i])
+                c.append(nums.count(a[i]))
+        print(b)
+        if len(b) == 0:
+            return -1
+        z = max(c)
+        y = c.index(z)
+        print(y)
+        return b[y]
