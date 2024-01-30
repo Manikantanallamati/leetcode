7350cc2dabb1590254563197eef47d03300199e7
Manikantanallamati <140334586+Manikantanallamati@users.noreply.github.com>
2024-01-30 11:42:48 +0530

    Time: 116 ms (31.49%) | Memory: 16.9 MB (60.78%) - LeetSync

--- 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
+++ 745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.py
@@ -1,6 +1,5 @@
 class Solution:
     def nextGreatestLetter(self, let: List[str], tar: str) -> str:
-        li = []
         for i in range(len(let)):
             if ord(let[i]) > ord(tar):
                 return let[i]
