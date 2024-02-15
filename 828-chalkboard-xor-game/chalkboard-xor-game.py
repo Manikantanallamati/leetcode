06a50c70e9e2d6f941bbe1926d1ef89b1acc8b1e
Manikantanallamati <140334586+Manikantanallamati@users.noreply.github.com>
2024-02-15 22:15:20 +0530

    Time: 77 ms (22.83%) | Memory: 16.9 MB (67.39%) - LeetSync

--- 828-chalkboard-xor-game/chalkboard-xor-game.py
+++ 828-chalkboard-xor-game/chalkboard-xor-game.py
@@ -1,9 +1,11 @@
 class Solution:
     def xorGame(self, nums: List[int]) -> bool:
+        if len(nums)%2==0:
+            return True
         xor = nums[0]
         for i in range(1,len(nums)):
             xor ^= nums[i] 
-        if xor == 0 or len(nums)%2==0:
+        if xor == 0 :
             return True
         else:
             return False
